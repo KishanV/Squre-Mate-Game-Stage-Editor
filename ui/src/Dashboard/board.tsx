c9b5ed94e30a7a9b51ee3e121b34d7a16fddec18
kishan devani <spidren.inc@gmail.com>
2018-10-21 03:33:41 +0530

    Screen update.

---
+++ ui/src/Dashboard/board.tsx
@@ -0,0 +1,11 @@
+export class Dashboard extends React.Component<any,any> {
+    constructor(props:any) {
+        super(props);
+    }
+
+    render() {
+        return (<div className={'Dashboard'}>
+            <div className={'Screens'}></div>
+        </div>);
+    }
+}
--- ui/src/StageBoard/index.tsx
+++
@@ -1,3 +0,0 @@
-export class StageBoard extends React.Component<any,any> {
-
-}
