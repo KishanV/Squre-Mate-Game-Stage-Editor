a9f51d078ad3811c8e13009e0ddeff237a1f589f
jadvani75 <jadvani75@gmail.com>
2018-10-21 18:33:09 +0530

    fix for mac
    fixed path bug in mac OS.

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,16 +5,15 @@ import {Express} from "express";
 
 export class Server {
 
-    public express:Express;
-    public port:number = 786;
-    public router:core.Router;
-    public currentPath:string;
+    public express: Express;
+    public port: number = 786;
+    public router: core.Router;
+    public currentPath: string;
 
-    constructor () {
+    constructor() {
         this.express = express();
         this.mountRoutes();
-
-        this.express.listen(this.port, (err:any) => {
+        this.express.listen(this.port, (err: any) => {
             if (err) {
                 return console.log(err)
             }
@@ -22,29 +21,36 @@ export class Server {
         });
     }
 
-    private mountRoutes (): void {
+    private mountRoutes(): void {
         this.router = express.Router();
         const router = this.router;
-        let folderList = __dirname.split('\\');
-        folderList.length = folderList.length - 2;
-
+        let folderList: any = __dirname.split('\\');
+        if (folderList.length == 1) {
+            folderList = __dirname.split('/');
+            folderList.length = folderList.length - 2;
+            folderList.push("ui");
+            folderList = folderList.join('/');
+        } else {
+            folderList.length = folderList.length - 2;
+            folderList.push("ui");
+            folderList = folderList.join('\\');
+        }
 
         router.use('/dist', function (req, res, next) {
             const url = req.url;
-            const extJs = url.substr(url.length-".js".length);
-            const extMap = url.substr(url.length-".js.map".length);
-            if(extJs != ".js" && extMap != ".js.map"){
+            const extJs = url.substr(url.length - ".js".length);
+            const extMap = url.substr(url.length - ".js.map".length);
+            if (extJs != ".js" && extMap != ".js.map") {
                 req.url = req.url + ".js";
             }
+            console.log('uri -> ' + req.url);
             next()
         });
-        router.use('',express.static(folderList.join('\\')+ '\\ui'));
-
-
+        router.use('', express.static(folderList));
         router.use(bodyParser.json());
         router.use(bodyParser.urlencoded({
             extended: true,
-            limit : '50mb'
+            limit: '50mb'
         }));
         this.express.use('/', router);
     }
