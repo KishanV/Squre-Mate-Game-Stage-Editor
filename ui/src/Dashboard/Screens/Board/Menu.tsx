90f366223b3e86775ac0765ce64725378d2c631c
jadvani75 <jadvani75@gmail.com>
2018-10-23 02:53:45 +0530

    Added selection functionality to menu buttons.

--- ui/src/Dashboard/Screens/Board/Menu.tsx
+++ ui/src/Dashboard/Screens/Board/Menu.tsx
@@ -1,17 +1,53 @@
+import {Card} from "./Card";
+
 export class Menu extends React.Component<any, any> {
+    state: any = {
+        Sel: 1
+    };
 
     constructor(props: any) {
         super(props);
+        this.selectMenu = this.selectMenu.bind(this);
+    }
+
+    selectMenu(no: number) {
+        this.setState({
+            Sel: 3
+        });
+    }
+
+    getItemMenu() {
+        const list: any[] = [];
+        for (let i = 0; i < 4; i++) {
+            let str = 'Sel';
+            if (i + 1 == this.state.Sel) {
+
+                list.push(<div className={'Item ' + str} onClick={ event1 => {
+                        this.setState({
+                            Sel: 1 + i
+                        });
+                }}>0{i + 1}</div>);
+
+            } else {
+
+                list.push(<div className={'Item '} onClick={event1 => {
+                    this.setState({
+                        Sel: 1 + i
+                    });
+                }}>0{i + 1}</div>);
+
+            }
+
+        }
+        return list;
+
     }
 
     render() {
         return (
             <div className={'Menu'}>
                 <div className={'Holder'}>
-                    <div className={'Item Sel'}>01</div>
-                    <div className={'Item'}>02</div>
-                    <div className={'Item'}>03</div>
-                    <div className={'Item'}>04</div>
+                    {this.getItemMenu()}
                 </div>
             </div>
         );
