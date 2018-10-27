6b7beebfd0bfd0c2cc0c8991964dcd69028c8fa4
kishan devani <spidren.inc@gmail.com>
2018-10-27 21:58:19 +0530

    Update.

--- ui/src/Dashboard/Screens/Editor/Editor.tsx
+++ ui/src/Dashboard/Screens/Editor/Editor.tsx
@@ -9,7 +9,7 @@ export class Editor extends React.Component<any, any> {
     getBackgroundBoxes() {
         const list = [];
         for (let i = 0; i < 35; i++) {
-            list.push(<div className={'Box'}></div>)
+            list.push(<div className={'Box'} key={i}></div>)
         }
         return list;
     }
@@ -17,7 +17,9 @@ export class Editor extends React.Component<any, any> {
     getPlaygroundBoxes() {
         const list = [];
         for (let i = 0; i < 35; i++) {
-            list.push(<div className={'Box'}></div>)
+            list.push(<div className={'Box'} key={i}>
+                <div className={'Border'}></div>
+            </div>)
         }
         return list;
     }
@@ -31,22 +33,23 @@ export class Editor extends React.Component<any, any> {
                     </svg>
                     <span className={'lable'}>play</span>
                 </div>
-                <div className={'MenuIcon'}>
+                <div className={'Menu'}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                         <path d="M0 0h24v24H0z" fill="none"/>
                         <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" fill="#ffd210"/>
                     </svg>
                 </div>
-                <div className={'Menu'}></div>
+                <div className={'Lable'}>Lavel 1</div>
             </div>
             <div className={'Boxes'}>
-                <div className={'Playground'}>
-                    {this.getPlaygroundBoxes()}
-                </div>
                 <div className={'Background'}>
                     {this.getBackgroundBoxes()}
                 </div>
+                <div className={'Playground'}>
+                    {this.getPlaygroundBoxes()}
+                </div>
             </div>
+            <div className={'Save'}>Save</div>
         </div>);
     }
 
